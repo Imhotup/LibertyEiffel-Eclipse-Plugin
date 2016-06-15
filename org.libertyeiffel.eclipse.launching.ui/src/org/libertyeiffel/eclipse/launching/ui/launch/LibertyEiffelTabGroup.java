83b7d5a16dde94787d72317c056296aff211a3b8
Watad Wallah <nyah.check@ubuea.cm>
2016-06-15 15:04:19 +0100

    implementing Liberty Eiffel compiler

---
+++ org.libertyeiffel.eclipse.launching.ui/src/org/libertyeiffel/eclipse/launching/ui/launch/LibertyEiffelTabGroup.java
@@ -0,0 +1,21 @@
+package org.libertyeiffel.eclipse.launching.ui.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+public class LibertyEiffelTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+				new LibertyEiffelMainTab(),
+				new LibertyEiffelClasspathTab(),
+				new CommonTab()
+		};
+		
+		setTabs(tabs);
+	}
+
+}
