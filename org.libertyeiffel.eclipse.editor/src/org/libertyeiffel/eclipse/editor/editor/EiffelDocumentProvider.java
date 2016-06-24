8d09f0f6e59ef33ea3d2eabb8e0675ee6c492d0b
Watad Wallah <nyah.check@ubuea.cm>
2016-06-24 19:46:08 +0100

    changed FileDocumentProvider to TextFileDocumentProvider

--- org.libertyeiffel.eclipse.editor/src/org/libertyeiffel/eclipse/editor/editor/EiffelDocumentProvider.java
+++ org.libertyeiffel.eclipse.editor/src/org/libertyeiffel/eclipse/editor/editor/EiffelDocumentProvider.java
@@ -2,31 +2,54 @@ package org.libertyeiffel.eclipse.editor.editor;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IDocumentExtension3;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
-import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 import org.libertyeiffel.eclipse.editor.EiffelEditorPlugin;
 
-public class EiffelDocumentProvider extends FileDocumentProvider {
-
-	@Override
-	protected IDocument createDocument(Object element) throws CoreException {
-		IDocument document = super.createDocument(element);
+public class EiffelDocumentProvider extends TextFileDocumentProvider {
+	
+	public EiffelDocumentProvider() {
 		
-		if (document instanceof IDocumentExtension3) {
-			IDocumentExtension3 extension3 = (IDocumentExtension3) document;
-			IDocumentPartitioner partitioner = new FastPartitioner(EiffelEditorPlugin
-					.getDefault().getPartitionScanner(), EiffelPartitionScanner.PARTITION_TYPE);
-			extension3.setDocumentPartitioner(EiffelEditorPlugin.MY_PARTITIONING, partitioner);
-			partitioner.connect(document);
-		}
-		
-		return document;
 	}
 	
 	@Override
-	protected IDocument createEmptyDocument() {
-		return new EiffelDocument();
+	protected FileInfo createFileInfo(Object element) throws CoreException{
+		FileInfo info = super.createFileInfo(element);
+		
+		if (info == null) {
+			info = createEmptyFileInfo();
+		}
+		
+		IDocument document = info.fTextFileBuffer.getDocument();
+		
+		if (document != null) {
+			IDocumentPartitioner partitioner = new FastPartitioner(EiffelEditorPlugin
+					.getDefault().getPartitionScanner(), EiffelPartitionScanner.PARTITION_TYPE);
+			partitioner.connect(document);
+			document.setDocumentPartitioner(partitioner);
+		}
+		
+		return info;
 	}
+
+//	@Override
+//	protected IDocument createDocument(Object element) throws CoreException {
+//		IDocument document = super.createDocument(element);
+//		
+//		if (document instanceof IDocumentExtension3) {
+//			IDocumentExtension3 extension3 = (IDocumentExtension3) document;
+//			IDocumentPartitioner partitioner = new FastPartitioner(EiffelEditorPlugin
+//					.getDefault().getPartitionScanner(), EiffelPartitionScanner.PARTITION_TYPE);
+//			extension3.setDocumentPartitioner(EiffelEditorPlugin.MY_PARTITIONING, partitioner);
+//			partitioner.connect(document);
+//		}
+//		
+//		return document;
+//	}
+//	
+//	@Override
+//	protected IDocument createEmptyDocument() {
+//		return new EiffelDocument();
+//	}
 }
--- org.libertyeiffel.eclipse.editor/src/org/libertyeiffel/eclipse/editor/editor/EiffelEditor.java
+++ org.libertyeiffel.eclipse.editor/src/org/libertyeiffel/eclipse/editor/editor/EiffelEditor.java
@@ -1,9 +1,5 @@
 package org.libertyeiffel.eclipse.editor.editor;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.jface.text.source.IVerticalRuler;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.editors.text.TextEditor;
 
 public class EiffelEditor extends TextEditor {
@@ -12,27 +8,22 @@ public class EiffelEditor extends TextEditor {
 		super();
 		
 		setSourceViewerConfiguration(new EiffelSourceViewerConfiguration());
-		//setDocumentProvider(new EiffelDocumentProvider());
+		setDocumentProvider(new EiffelDocumentProvider());
 	}
-
+	
 	@Override
-	protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
-		ISourceViewer viewer = new EiffelSourceViewer(parent, ruler, getOverviewRuler(),
-				isOverviewRulerVisible(), styles, this);
-		
-		//ensure decoration support has been created and configured.
-		getSourceViewerDecorationSupport(viewer);
-		
-		return viewer;
-	}
-	
-	public void doSave(IProgressMonitor monitor) {
-		super.doSave(monitor);
-		// captured a file save, handle anything else
-		System.out.println("saving file: " + this.getEditorInput().getName());
-	}
-	
 	public void dispose() {
 		super.dispose();
 	}
+
+//	@Override
+//	protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
+//		ISourceViewer viewer = new EiffelSourceViewer(parent, ruler, getOverviewRuler(),
+//				isOverviewRulerVisible(), styles, this);
+//		
+//		//ensure decoration support has been created and configured.
+//		getSourceViewerDecorationSupport(viewer);
+//		
+//		return viewer;
+//	}
 }
