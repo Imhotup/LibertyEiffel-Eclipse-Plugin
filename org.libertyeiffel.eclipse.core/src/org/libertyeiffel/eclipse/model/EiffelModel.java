6a18f70829f487201b042961626557bb0a180373
Watad Wallah <nyah.check@ubuea.cm>
2016-06-24 17:52:26 +0100

    EiffelModel.java
    .

---
+++ org.libertyeiffel.eclipse.core/src/org/libertyeiffel/eclipse/model/EiffelModel.java
@@ -0,0 +1,21 @@
+package org.libertyeiffel.eclipse.model;
+
+import java.util.List;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+
+public class EiffelModel {
+
+	public static EiffelModel getModel(IDocument document, Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void getContentProposals(String prefix, String indent, int offset, 
+			List<ICompletionProposal> proposals) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
