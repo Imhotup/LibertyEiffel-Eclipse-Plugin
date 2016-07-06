f3544d9dcaf958fe28e62e61631b95da0d2c84f5
Watad Wallah <nyah.check@ubuea.cm>
2016-07-06 19:01:26 +0100

    Get Environment Variables

---
+++ org.libertyeiffel.eclipse.launching/src/org/libertyeiffel/eclipse/launching/launcher/util/Constants.java
@@ -0,0 +1,10 @@
+package org.libertyeiffel.eclipse.launching.launcher.util;
+
+public class Constants {
+
+	public static final String LAUNCH_CONFIGURATION_TYPE = 
+			"org.libertyeiffel.eclipse.launch.EiffelConfigurationType";
+	
+	public static final String ATTR_ENVIRONMENT_VARIABLES = "attr_environment_variables";
+	
+}
