50bb3cc0c1e637c9bf29d89fbd08123f20f483c9
Watad Wallah <nyah.check@ubuea.cm>
2016-07-18 14:10:18 +0100

    classpath

--- org.libertyeiffel.eclipse.launching/src/org/libertyeiffel/eclipse/launching/launcher/EiffelLaunchDelegate.java
+++ org.libertyeiffel.eclipse.launching/src/org/libertyeiffel/eclipse/launching/launcher/EiffelLaunchDelegate.java
@@ -110,149 +110,149 @@ public class EiffelLaunchDelegate implements ILaunchConfigurationDelegate {
 		return null;
 	}
 	
-	
-	 /* Returns whether the contents of this launch configuration are
-	  *  equal to the contents of the given launch configuration.
-	  */
-	@Override
-	boolean	contentsEqual(ILaunchConfiguration configuration) {
-		// TODO
-		return false;
-	}
-	
-	/* Returns a copy of this launch configuration, as a working copy, with the specified name.
-	 */
-	@Override
-	ILaunchConfigurationWorkingCopy	copy(String name) {
-		//TODO
-		return null;
-	}
-
-	/* Deletes this launch configuration.
-	 */
-	@Override
-	void delete() {
-		//TODO
-	}
-
-	/* Returns whether this launch configuration's underlying storage exists. */
-	@Override
-	boolean	exists() {
-		//TODO
-		return false;
-	}
-
-	/* Returns the boolean-valued attribute with the given name. */
-	boolean	getAttribute(String attributeName, boolean defaultValue) {
-		//TODO
-		return false;
-	}
-
-	/* Returns the integer-valued attribute with the given name.*/
-	int	getAttribute(String attributeName, int defaultValue) {
-		//TODO
-		return false;
-	}
-
-	/* Returns the java.util.Map-valued attribute with the given name.*/
-	Map<String,String>	getAttribute(String attributeName, Map<String,String> defaultValue) {
-		//TODO
-		return null;
-	}
-
-	/* Returns this launch configuration's type's category, or null if unspecified. */
-	String	getCategory() {
-		//TODO
-		return null;
-	}
-
-	/* Returns the file this launch configuration is stored in, or null if this configuration is stored locally with the workspace. */
-	IFile	getFile() {
-		//TODO
-		return null;
-	}
-
-	/* Returns the resources this launch configuration is associated with or null if none. */
-	IResource[]	getMappedResources() {
-		//TODO
-		return null;
-	}
-
-	/* Returns a memento for this launch configuration, or null if unable to generate a memento for this configuration. */
-	String	getMemento() {
-		//TODO
-		return null;
-	}
-
-	/* Returns the launch modes that have been set on this configuration. */
-	Set<String>	getModes() {
-		//TODO
-		return null;
-	}
-	
-	/* Returns the name of this launch configuration. */
-	String	getName() {
-		//TODO
-		return null;
-	}
-	
-	/* Returns the preferred launch delegate that has been set on this configuration or null if one is not specified. */
-	ILaunchDelegate	getPreferredDelegate(Set<String> modes) {
-		//TODO
-		return null;
-	}
-
-	/* Returns the type of this launch configuration. */
-	ILaunchConfigurationType	getType() {
-		//TODO
-		return null;
-	}
-
-	/* Returns a working copy of this launch configuration. */
-	ILaunchConfigurationWorkingCopy	getWorkingCopy() {
-		//TODO
-		return null;
-	}
-	
-	/* Returns whether this configuration contains an attribute with the given name. */
-	boolean	hasAttribute(String attributeName) {
-		//TODO
-		return false;
-	}
-	
-	/* Returns whether this launch configuration is stored locally within the workspace. */
-	boolean	isLocal() {
-		//TODO
-		return false;
-	}
-
-	/* Returns whether this launch configuration is a candidate for migration. */
-	boolean	isMigrationCandidate() {
-		//TODO
-		return false;
-	}
-
-	/* Returns whether this launch configuration is read-only. */
-	boolean	isReadOnly() {
-		//TODO
-		return false;
-	}
-
-	/* Returns whether this launch configuration is a working copy. */
-	boolean	isWorkingCopy() {
-		//TODO
-		return false;
-	}
-
-	/* Migrates this launch configuration to be compatible with current tooling. */
-	void migrate() {
-		//TODO
-	}
-
-	/* Returns whether this launch configuration supports the specified mode. */
-	boolean	supportsMode(String mode) {
-		//TODO
-		return false;
-	}
+//	
+//	 /* Returns whether the contents of this launch configuration are
+//	  *  equal to the contents of the given launch configuration.
+//	  */
+//	@Override
+//	boolean	contentsEqual(ILaunchConfiguration configuration) {
+//		// TODO
+//		return false;
+//	}
+//	
+//	/* Returns a copy of this launch configuration, as a working copy, with the specified name.
+//	 */
+//	@Override
+//	ILaunchConfigurationWorkingCopy	copy(String name) {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Deletes this launch configuration.
+//	 */
+//	@Override
+//	void delete() {
+//		//TODO
+//	}
+//
+//	/* Returns whether this launch configuration's underlying storage exists. */
+//	@Override
+//	boolean	exists() {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Returns the boolean-valued attribute with the given name. */
+//	boolean	getAttribute(String attributeName, boolean defaultValue) {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Returns the integer-valued attribute with the given name.*/
+//	int	getAttribute(String attributeName, int defaultValue) {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Returns the java.util.Map-valued attribute with the given name.*/
+//	Map<String,String>	getAttribute(String attributeName, Map<String,String> defaultValue) {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns this launch configuration's type's category, or null if unspecified. */
+//	String	getCategory() {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns the file this launch configuration is stored in, or null if this configuration is stored locally with the workspace. */
+//	IFile	getFile() {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns the resources this launch configuration is associated with or null if none. */
+//	IResource[]	getMappedResources() {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns a memento for this launch configuration, or null if unable to generate a memento for this configuration. */
+//	String	getMemento() {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns the launch modes that have been set on this configuration. */
+//	Set<String>	getModes() {
+//		//TODO
+//		return null;
+//	}
+//	
+//	/* Returns the name of this launch configuration. */
+//	String	getName() {
+//		//TODO
+//		return null;
+//	}
+//	
+//	/* Returns the preferred launch delegate that has been set on this configuration or null if one is not specified. */
+//	ILaunchDelegate	getPreferredDelegate(Set<String> modes) {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns the type of this launch configuration. */
+//	ILaunchConfigurationType	getType() {
+//		//TODO
+//		return null;
+//	}
+//
+//	/* Returns a working copy of this launch configuration. */
+//	ILaunchConfigurationWorkingCopy	getWorkingCopy() {
+//		//TODO
+//		return null;
+//	}
+//	
+//	/* Returns whether this configuration contains an attribute with the given name. */
+//	boolean	hasAttribute(String attributeName) {
+//		//TODO
+//		return false;
+//	}
+//	
+//	/* Returns whether this launch configuration is stored locally within the workspace. */
+//	boolean	isLocal() {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Returns whether this launch configuration is a candidate for migration. */
+//	boolean	isMigrationCandidate() {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Returns whether this launch configuration is read-only. */
+//	boolean	isReadOnly() {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Returns whether this launch configuration is a working copy. */
+//	boolean	isWorkingCopy() {
+//		//TODO
+//		return false;
+//	}
+//
+//	/* Migrates this launch configuration to be compatible with current tooling. */
+//	void migrate() {
+//		//TODO
+//	}
+//
+//	/* Returns whether this launch configuration supports the specified mode. */
+//	boolean	supportsMode(String mode) {
+//		//TODO
+//		return false;
+//	}
 
 }
