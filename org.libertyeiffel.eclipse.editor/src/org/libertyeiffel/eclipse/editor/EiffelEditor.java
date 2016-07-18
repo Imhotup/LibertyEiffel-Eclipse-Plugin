b8958c0d5bafe27284876eecbde63ee964e8fac3
Watad Wallah <nyah.check@ubuea.cm>
2016-07-18 16:11:16 +0100

    resolve EiffelSourceViewer constructor conflict

--- org.libertyeiffel.eclipse.editor/src/org/libertyeiffel/eclipse/editor/EiffelEditor.java
+++ org.libertyeiffel.eclipse.editor/src/org/libertyeiffel/eclipse/editor/EiffelEditor.java
@@ -1,29 +1,13 @@
 package org.libertyeiffel.eclipse.editor;
 
-import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.jface.text.source.IVerticalRuler;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.editors.text.TextEditor;
 
 public class EiffelEditor extends TextEditor {
 	
 	public EiffelEditor() {
 		super();
-		
+		setSourceViewerConfiguration(new EiffelSourceViewerConfiguration());
 		setDocumentProvider(new EiffelDocumentProvider());
-	}
-
-	@Override
-	protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
-		ISourceViewer viewer = new EiffelSourceViewer(parent, ruler, getOverviewRuler(),
-				isOverviewRulerVisible(), styles, this);
-		
-		//ensure decoration support has been created and configured.
-		getSourceViewerDecorationSupport(viewer);
-		
-		//more custom stuff
-		
-		return viewer;
 	}
 	
 	@Override
